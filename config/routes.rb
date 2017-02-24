e9958466c371943806a714a4419d2002f166ae7b
photofinish <dangs456789@sina.com>
2017-02-24 15:03:31 +0800

    init devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
 
-  resources :atricles
+  devise_for :users, controllers: {
+      sessions: 'users/sessions',
+      registrations: "users/registrations"
+  }
+  get 'users/:user_id/atricles', to: 'atricles#index'
+  resources :atricles do
+    resources :histories
+  end
+  resources :photos
   root 'atricles#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
